84e7cf73685448607e5bad8e2dd1d32c6d6a8c14
Marc Benedict C. Escatron <marc.escatron07@gmail.com>
2020-12-23 09:49:42 +0800

    corrected import paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
 import { SuccessAlert } from 'sub-components/alerts/success/success.component';
 import { WarningAlert } from 'sub-components/alerts/warning/warning.component';
 
